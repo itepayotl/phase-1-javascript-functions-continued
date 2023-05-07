47612347a64461349ed481ee6c492ccd45efedc0
Israel <itepayotl@yahoo.com>
2023-05-06 21:57:27 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // code your solution here
+function saturdayFun(activity = 'roller-skate'){
+    return `This Saturday, I want to ${activity}!` 
+}
+function mondayWork(work = "go to the office") {
+    return `This Monday, I will ${work}.`
+}
+function wrapAdjective(all = "*"){
+    return function (how = "special"){
+        return `You are ${all}${how}${all}!`;
+    }
+}
